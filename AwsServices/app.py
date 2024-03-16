949f4489dd7ff570168c98359643599038e93c36
Sunehildeep <sunehildeepsinghsandhu123@gmail.com>
2024-03-16 19:09:51 -0400

    Updated dotenv

--- AwsServices/app.py
+++ AwsServices/app.py
@@ -2,11 +2,13 @@ from chalice import Chalice
 import boto3
 from database.dynamodb import DynamoDB
 
+
 app = Chalice(app_name='AwsServices')
 
 # Calling the DynamoDB class to create the table
 dynamo_resource = boto3.resource('dynamodb')
 dynamo_db = DynamoDB(dynamo_resource)
+
 
 @app.route('/sign-up', methods=['POST'], cors=True)
 def sign_up():
@@ -15,10 +17,12 @@ def sign_up():
     print(body)
     return dynamo_db.signUp(body)
 
+
 @app.route('/login', methods=['POST'], cors=True)
 def login():
     request = app.current_request
     return dynamo_db.login(request.json_body)
+
 
 @app.route('/addCommunity', methods=['PUT'], cors=True)
 def index():
@@ -26,7 +30,7 @@ def index():
     dynamo_db.putCommunity(request.json_body)
     return {'message': 'Community added successfully!'}
 
+
 @app.route('/getCommunity/{user_name}', methods=['GET'], cors=True)
 def get_community(user_name):
     return dynamo_db.getCommunity(user_name)
-
--- chat/main.py
+++ chat/main.py
@@ -5,40 +5,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-secretKey = os.getenv("SECRET_KEY")
+secretKey = os.getenv(".", "SECRET_KEY")
 FE_URL = os.getenv("FE_URL")
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = secretKey
-socketio = SocketIO(app, cors_allowed_origins=FE_URL , cors_allowed_methods=["GET", "POST"], cors_allowed_headers="*")
-
-@socketio.on('connect')
-def test_connect():
-    print('Client connected')
-
-@socketio.on('join_community')
-def join_community(data):
-    user_id = data['user_id']
-    community_id = data['community_id']
-    if user_id and community_id:
-        # TODO : Add user to community
-        send(str(user_id) + ' has entered the room.', to=community_id)
-
-@socketio.on('leave_community')
-def on_leave(data):
-    user_id = data['user_id']
-    community_id = data['community_id']
-    # TODO : Remove user from community
-    send(user_id + ' has left the room.', to=community_id)
-
-@socketio.on('message_send')
-def handle_message(data):
-    user_id = data['user_id']
-    community_id = data['community_id']
-    message = data['message']
-    if user_id and community_id and message:
-        # TODO : Save message to database
-        emit('new_msg', {'user_id': user_id, 'community_id': community_id, 'message': message}, broadcast=True)
+socketio = SocketIO(app)
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
